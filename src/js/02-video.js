440b16fc8bab079903f2f122de2ae4513ddcd48e
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2022-01-31 14:22:05 +0200

    data replacement

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+
+const iframeRef = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeRef);
+
+player.on('timeupdate', throttle(onPlayVideo, 1000));
+    
+function onPlayVideo(data) {
+    let timeCurrent = Math.round(data.seconds);
+    localStorage.setItem('videoplayer-current-time', timeCurrent);
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+onPageLoad();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit)
+
+function onFormInput(e) { 
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+    
+    const savedOfForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        
+    if (savedOfForm === null) { 
+        return
+    };
+    
+    console.log('данные ', savedOfForm);
+    console.log('email', savedOfForm.email);
+    console.log('message', savedOfForm.message);
+
+    localStorage.removeItem(STORAGE_KEY);
+    e.currentTarget.reset();
+};
+
+function onPageLoad() { 
+    const savedOfForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    
+    if (savedOfForm === null) { 
+        return
+    };
+        
+    formRef.elements.email.value = savedOfForm.email;
+    formRef.elements.message.value = savedOfForm.message;   
+};
